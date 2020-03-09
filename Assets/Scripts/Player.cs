7736934e707cfa7ea463361bef8d23ab6377802d
ryan_tran <ryantran2165@gmail.com>
2020-03-08 23:11:40 -0700

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int thiccLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addThicc(int thicc)
+    {
+        thiccLevel += thicc;
+        Debug.Log("Thicc: " + thiccLevel);
+    }
+
+}
