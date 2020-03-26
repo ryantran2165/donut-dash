487a4f1e86010a636260ec01b7a767ccb1e9856a
ryan_tran <ryantran2165@gmail.com>
2020-03-26 00:47:23 -0700

    Fix blurry assets, increase min light distance

--- Assets/Scripts/Managers/BackgroundManager.cs
+++ Assets/Scripts/Managers/BackgroundManager.cs
@@ -20,7 +20,7 @@ public class BackgroundManager : MonoBehaviour
     private float lastSpawnX;
     private float nextSpawnInterval;
     private float lastLightX;
-    private const float MIN_LIGHT_INTERVAL = 5f;
+    private const float MIN_LIGHT_INTERVAL = 15f;
 
     private const float LAYER_1_SPEED = 0.7f;
     private const float LAYER_2_SPEED = 0.5f;
